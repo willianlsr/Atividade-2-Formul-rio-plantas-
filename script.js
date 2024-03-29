61c0554c3eadf8fa18091fded70e98628df0da31
Willian Lenon <willian_lsr@hotmail.com>
2024-03-29 17:22:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function salvar(){
+
+    let form = document.getElementById("form-cadastro")
+    let nome = form.NomePlanta.value
+    let especie = form.EspeciePlanta.value
+    let altura = form.AlturaMaxima.value
+
+    if(nome.trim() == ""){
+        alert("Preencha o nome da planta:")
+        return false
+        }
+        if (especie.trim() == ""){
+            alert("Preencha a espécie da planta:")
+            return false
+        }
+        if (altura.trim() == ""){
+            alert("Preencha a altura máxima da planta:")
+            return false
+        }
+        alert("CADASTRADO COM SUCESSO")
+
+
+}
+
+
